7a80d5eb8e0e1be8ee0263f01d511f776f1fd695
504 <504@DESKTOP-MQIVQL4>
2022-03-28 18:05:43 +0900

    1

--- src/한요한/main.java
+++ src/한요한/main.java
@@ -465,26 +465,26 @@ public class main {
 	
 } // c e
 
-////////////////////////////////////////////인기글출력
-for(int i = 0; i < Controller.카테고리.length ; i++) {
-ArrayList<Board> 인기글 = Controller.인기글(Controller.카테고리[i]); // 각 카테고리 인기글 가져오기
-System.out.println("---------------------카테고리: " + Controller.카테고리[i]+"-----------------------------");
-System.out.println("제목\t\t내용\t\t글번호\t\t추천수");
-for(int j = 0; j < 인기글.size() ; j++) {
-if(인기글.size() != 0) {
-String 내용 = "";
-for(int s = 0; s < 인기글.get(j).getContent().length() ; s++) {
-내용 += 인기글.get(j).getContent().charAt(s);
-if(s == 9) {
-break;
-}
-}
-System.out.println(인기글.get(j).getTitle()+ "\t\t"+ String.format("%-15s", 내용) + 인기글.get(j).getIndex() +"\t\t"+ 인기글.get(j).getGood());	
-
-}
-if(i == 4) {
-break;
-}
-}
-}
-///////////////////////////////////////////////////
+//////////////////////////////////////////////인기글출력
+//for(int i = 0; i < Controller.카테고리.length ; i++) {
+//ArrayList<Board> 인기글 = Controller.인기글(Controller.카테고리[i]); // 각 카테고리 인기글 가져오기
+//System.out.println("---------------------카테고리: " + Controller.카테고리[i]+"-----------------------------");
+//System.out.println("제목\t\t내용\t\t글번호\t\t추천수");
+//for(int j = 0; j < 인기글.size() ; j++) {
+//if(인기글.size() != 0) {
+//String 내용 = "";
+//for(int s = 0; s < 인기글.get(j).getContent().length() ; s++) {
+//내용 += 인기글.get(j).getContent().charAt(s);
+//if(s == 9) {
+//break;
+//}
+//}
+//System.out.println(인기글.get(j).getTitle()+ "\t\t"+ String.format("%-15s", 내용) + 인기글.get(j).getIndex() +"\t\t"+ 인기글.get(j).getGood());	
+//
+//}
+//if(i == 4) {
+//break;
+//}
+//}
+//}
+/////////////////////////////////////////////////////
